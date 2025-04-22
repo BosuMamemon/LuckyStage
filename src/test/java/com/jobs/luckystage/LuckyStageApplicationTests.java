9f0c7c30219bd0fb1e1a0eba33df91c293ac2b07
DESKTOP-A1PALJT\조동혁 <realisshobeen@gamil.com>
2025-04-22 10:30:54 +0900

    test

---
+++ src/test/java/com/jobs/luckystage/LuckyStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobs.luckystage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyStageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
