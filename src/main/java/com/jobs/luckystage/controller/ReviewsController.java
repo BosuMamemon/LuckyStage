ac8684b0d6b4ce3486726a5e25d6ec002dbd2ed0
DESKTOP-R4K628H\it <realisshobeen@gmail.com>
2025-04-23 14:10:43 +0900

    2025-04-23 14:10 조유빈

---
+++ src/main/java/com/jobs/luckystage/controller/ReviewsController.java
@@ -0,0 +1,12 @@
+package com.jobs.luckystage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/reviews")
+public class ReviewsController {
+    @GetMapping("/dksl")
+    public void addReview(){}
+}
