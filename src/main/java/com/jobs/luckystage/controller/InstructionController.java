b76368c2ab5aae70f33425e3aa1f74c8798dd7c9
DESKTOP-R4K628H\it <realisshobeen@gmail.com>
2025-04-25 17:00:52 +0900

    2025-04-25 17:00 조유빈 (이용안내틀)

---
+++ src/main/java/com/jobs/luckystage/controller/InstructionController.java
@@ -0,0 +1,15 @@
+package com.jobs.luckystage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/instructions")
+public class InstructionController {
+
+    @GetMapping("/instruction")
+    public String instructionPage() {
+        return "instructions/instruction"; // ← 경로 그대로
+    }
+}
