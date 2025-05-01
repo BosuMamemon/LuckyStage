26dd051f5f1967f15b8086f1d09d4031e9375e3d
DESKTOP-R4K628H\it <realisshobeen@gmail.com>
2025-05-01 11:47:11 +0900

    2025-05-01 11:46 조유빈 (추첨화면 연결, 예약내역 공연상세정보 연결)

--- src/main/java/com/jobs/luckystage/domain/Tickets.java
+++ src/main/java/com/jobs/luckystage/domain/Tickets.java
@@ -16,17 +16,22 @@ public class Tickets {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) //이거 디비에 공연정보가 아닌 회원정보가 쳐 넘어가길래 일단은 해놓았는데 삭제해도됨
     private long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "members_username") //추가
     private Members members;
+
     @CreationTimestamp
     @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime regDate;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime selectedDate;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "concerts_concert_num") //추가
     private Concerts concerts;
+
     @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private Date lotteryDate;
 
